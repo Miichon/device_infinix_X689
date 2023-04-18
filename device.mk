c6b6f22dfe4e8890e61bb5ec67ee61eb439339d4
Miichon <Zksync@Miichon.ovh>
2023-04-18 16:15:20 +0700

    test
    Signed-off-by: Miichon <Zksync@Miichon.ovh>

--- device.mk
+++ device.mk
@@ -69,15 +69,13 @@ PRODUCT_PACKAGES += \
     update_engine_sideload
 
 # VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_TARGET_VNDK_VERSION := 31
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
-
-TW_LOAD_VENDOR_MODULES := "chipone-tddi.bin ilitek_ts_fw.bin mt6631_fm_v1_patch.bin mt6631_fm_v1_patch.bin"
 
 # Fastbootd
 PRODUCT_PACKAGES += \
