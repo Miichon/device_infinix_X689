74ddf2a27575f65bf08c7a1eb3793a7552ca2397
Miichon <124475801+Miichon@users.noreply.github.com>
2023-04-18 19:19:02 +0700

    Update omni_X689.mk

--- omni_X689.mk
+++ omni_X689.mk
@@ -32,7 +32,7 @@ $(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X689
-PRODUCT_NAME := twrp_X689
+PRODUCT_NAME := omni_X689
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Hot 10S
 PRODUCT_MANUFACTURER := Infinix
