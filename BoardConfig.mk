942b76e7e0cec4d22c06dc1ddf3c3465f5f62925
Miichon <Zksync@Miichon.ovh>
2023-04-23 20:21:31 +0700

    dt update
    Signed-off-by: Miichon <Zksync@Miichon.ovh>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,7 @@ TW_FRAMERATE := 60
 # Maintainer/Version
 TW_DEVICE_VERSION := Nomercy
 
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # logcd
